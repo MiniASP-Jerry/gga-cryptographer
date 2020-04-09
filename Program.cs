a108448bc2a04aa6cf247e75d85ee2e687e8d45e
陳玨鄅 Jerry <jerryc@miniasp.com>
2020-04-09 18:43:50 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace gga_cryptographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = args[0];
+            var toEncode = args[1];
+            var toDecode = args[2];
+            var crypto = new Cryptographer();
+            var encoded = crypto.AESEncript(toEncode, key);
+            var decoded = crypto.AESDecript(toDecode, key);
+            Console.WriteLine($"加密後：{encoded}");
+            Console.WriteLine($"加密後：{decoded}");
+        }
+    }
+}
